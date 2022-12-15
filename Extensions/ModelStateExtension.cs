536566998bd95fb84eaedff352c9f956e79ca295
Paula Morais <paula.morais@infraclearsalecom.onmicrosoft.com>
2022-12-15 01:13:18 -0300

    Add Model State Extension

---
+++ Extensions/ModelStateExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace AspNet_Core6.Fundamentals.Extensions
+{
+    public static class ModelStateExtension
+    {
+        public static List<string> GetErrors(this ModelStateDictionary modelState)
+        {
+            var resultErrors = new List<string>();
+
+            foreach (var value in modelState.Values)
+
+                resultErrors.AddRange(value.Errors.Select(error => error.ErrorMessage));
+            return resultErrors;
+        }
+    }
+}
