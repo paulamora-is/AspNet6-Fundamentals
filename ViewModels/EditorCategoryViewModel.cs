2de6d5583fc48efee0d5bb9b45892be6974136ba
Paula Morais <paula.morais@infraclearsalecom.onmicrosoft.com>
2023-07-16 18:52:05 -0300

    Add new ViewsModel

--- ViewModels/EditorCategoryViewModel.cs
+++ ViewModels/EditorCategoryViewModel.cs
@@ -7,6 +7,7 @@ namespace AspNet_Core6.Fundamentals.ViewModels
         [Required(ErrorMessage = "The 'Name' field is required.")]
         [StringLength(40, MinimumLength = 3, ErrorMessage = "This field must contain between three and forty characters")]
         public string Name { get; set; }
+
         [Required(ErrorMessage = "The 'Slug' field is required.")]
         public string Slug { get; set; }
     }
---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNet_Core6.Fundamentals.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Informe o e-mail")]
+        [EmailAddress(ErrorMessage = "O 'Email' é inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Informe a senha")]
+        public string Password { get; set; }
+    }
+}
