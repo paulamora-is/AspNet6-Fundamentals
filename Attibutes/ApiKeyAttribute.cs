ce7391603447473ad8e9a022c59420f4b61c9250
Paula Morais <paula.morais@infraclearsalecom.onmicrosoft.com>
2023-07-16 20:54:41 -0300

    Added ApyKeyAttribute

---
+++ Attibutes/ApiKeyAttribute.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AspNet_Core6.Fundamentals.Attibutes
+{
+    [AttributeUsage(validOn: AttributeTargets.Class | AttributeTargets.Method)]
+    public class ApiKeyAttribute : Attribute, IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.HttpContext.Request.Query.TryGetValue(Configuration.ApiKeyName, out var apiKey))
+            {
+
+                context.Result = new ContentResult
+                {
+                    StatusCode = 401,
+                    Content = "ApiKey not found"
+                };
+
+                return;
+            }
+
+            if (!Configuration.ApiKey.Equals(apiKey))
+            {
+                context.Result = new ContentResult
+                {
+                    StatusCode = 403,
+                    Content = "Unauthorized access"
+                };
+
+                return;
+            }
+
+            await next();
+        }
+    }
+}
---
+++ Configuration.cs
@@ -0,0 +1,9 @@
+﻿namespace AspNet_Core6.Fundamentals
+{
+    public static class Configuration
+    {
+        public static string JwtKey { get; set; } = "";
+        public static string ApiKeyName { get; set; } = "api_key";
+        public const string ApiKey = "";
+    }
+}
