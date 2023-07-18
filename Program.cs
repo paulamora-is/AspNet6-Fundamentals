5a46f224cda3df8063378801bd1534c5ade1a80c
Paula Morais <paula.morais@infraclearsalecom.onmicrosoft.com>
2023-07-17 21:17:22 -0300

    Organized methods

--- Program.cs
+++ Program.cs
@@ -7,36 +7,12 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
-
-var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = false,
-        ValidateActor = false
-    };
-});
-
-builder.Services.AddControllers()
-    .ConfigureApiBehaviorOptions(options =>
-    {
-        options.SuppressModelStateInvalidFilter = true;
-    });
-
-builder.Services.AddDbContext<BlogDataContext>();
-builder.Services.AddTransient<ITokenService, TokenService>();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+ConfigureAuthentication(builder);
+ConfigureMVC(builder);
+ConfigureServices(builder.Services);
 
 var app = builder.Build();
+LoadConfiguration(app);
 
 if (app.Environment.IsDevelopment())
 {
@@ -52,3 +28,52 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+static void LoadConfiguration(WebApplication app)
+{
+
+    Configuration.JwtKey = app.Configuration.GetValue<string>("JwtKey");
+    Configuration.ApiKeyName = app.Configuration.GetValue<string>("ApiKeyName");
+    Configuration.ApiKey = app.Configuration.GetValue<string>("ApiKey");
+
+    var smtp = new Configuration.SmtConfiguration();
+    app.Configuration.GetSection("Smtp").Bind(smtp);
+    Configuration.Smtp = smtp;
+}
+
+static void ConfigureAuthentication(WebApplicationBuilder builder)
+{
+    var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+    builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false,
+            ValidateActor = false
+        };
+    });
+}
+
+static void ConfigureMVC(WebApplicationBuilder builder)
+{
+    builder.Services.AddControllers()
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        options.SuppressModelStateInvalidFilter = true;
+    });
+}
+
+static void ConfigureServices(IServiceCollection services)
+{
+    services.AddDbContext<BlogDataContext>();
+    services.AddTransient<ITokenService, TokenService>();
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen();
+
+}
