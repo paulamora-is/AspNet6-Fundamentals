24b414319287cec3c326b4b05a732d9e21485b82
Paula Morais <paula.morais@infraclearsalecom.onmicrosoft.com>
2023-07-17 21:19:53 -0300

    Organized Configuration

--- Configuration.cs
+++ Configuration.cs
@@ -2,8 +2,17 @@
 {
     public static class Configuration
     {
-        public static string JwtKey { get; set; } = "";
-        public static string ApiKeyName { get; set; } = "api_key";
-        public const string ApiKey = "";
+        public static string JwtKey { get; set; }
+        public static string ApiKeyName { get; set; }
+        public static string ApiKey { get; set; }
+        public static SmtConfiguration Smtp { get; set; }
+
+        public class SmtConfiguration
+        {
+            public string Host { get; set; }
+            public int Port { get; set; }
+            public string UserName { get; set; }
+            public string Password { get; set; }
+        }
     }
 }
